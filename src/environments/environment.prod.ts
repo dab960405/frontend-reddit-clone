8dd86dcdb684963e51d6d54e8ded2d64538cb4fd
dab96 <diegobravo.c96@gmail.com>
2025-09-15 18:09:32 -0500

    change on environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true, 
-  apiUrl: 'https://backend-reddit-clone-q4pl.onrender.com/api', 
+  apiUrl: 'https://backend-reddit-clone-q4pl.onrender.com', 
   tinyApiKey: 'bsdfatxkximmxn8327z4wji36awu3oucow5zqt13z1290ysz' // Tu API key de TinyMCE
 };
